23e4f2bdb7255018c74f5977496418faa4fe91c8
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-10-20 21:24:25 -0400

    tried out palindrome cheat way

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,13 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+let reversed = str.split('').reverse().join('')
+if (reversed === str) {
+  return true
+}
+else {return false}
+
+}
 
 module.exports = palindrome;

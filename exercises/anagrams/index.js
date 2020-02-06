e49f175fb7d59929ff71608041bc447f618c0e6f
nhoxbongun <47346352+nhoxbongun@users.noreply.github.com>
2020-02-06 09:47:48 -0500

    redid anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,80 +9,44 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  // this is ok but we could make a function for it
- // const charMap1= {}
- // const charMap2= {}
- // const strA= stringA.replace(/[^\w]/g,'').toLowerCase()
- // const strB= stringB.replace(/[^\w]/g,'').toLowerCase()
- // for (let letter of  strA){
- //   letter.toLowerCase()
- //   charMap1[letter]= charMap1[letter]+1 || 1
- // }
- // for (let letter of  strB){
- //   letter.toLowerCase()
- //   charMap2[letter]= charMap2[letter]+1 || 1
- // }
-// check for equivalent
-
-const charMap1= buildCharMap(stringA)
-console.log(charMap1);
-const charMap2= buildCharMap(stringB)
-console.log(charMap2);
-
-var stringAProps = Object.keys(charMap1)
-var stringBProps = Object.keys(charMap2)
-
-
-
-// if(stringAProps.length !== stringBProps.length){
-// Basically can just use object key directly instead of assigning it to a variable which consumes more space
-if(Object.keys(charMap1).length !== Object.keys(charMap2).length){
-  return false
-}
-
-for(let key in stringAProps){
-  var propName= stringAProps[key]
-  if (charMap1[propName]!==charMap2[propName]){
-    return false
+  if (stringA === stringB) {
+    return true;
   }
 
-  return true
-}
-}
+  var charmap1 = {};
+  var charmap2 = {};
 
-function buildCharMap(str){
-  charMap= {}
-  for(let element of str.replace(/[^\w]/g,'').toLowerCase()){
-    charMap[element] = charMap[element]+1 || 1
+  for (let letter of stringA) {
+    charmap1[letter] = charmap1[letter] + 1 || 1;
   }
-  return charMap
+  for (let letter of stringB) {
+    charmap2[letter] = charmap2[letter] + 1 || 1;
+  }
+  if (Object.keys(charmap1).length !== Object.keys(charmap2).length) {
+    return false;
+  } else {
+    for (let key in charmap1) {
+      if (charmap1[key] !== charmap2[key]) {
+        false;
+      }
+      return true;
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = anagrams;
 
-
 // compare 2 sorted array to see if they are anagrams
 
-function sortAnagram(stringA,StringB){
-  return cleanString(stringA) === cleanString(stringB)
+function sortAnagram(stringA, StringB) {
+  return cleanString(stringA) === cleanString(stringB);
 }
 
-function cleanString(str){
-  return str.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('')
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
 }

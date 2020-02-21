a006578abbeac7ea1a3608567e321187849100c8
nhoxbongun <47346352+nhoxbongun@users.noreply.github.com>
2020-02-21 14:27:53 -0500

    hash map for counter pattern

--- my_exercises/counter_pattern/counter_pattern.js
+++ my_exercises/counter_pattern/counter_pattern.js
@@ -1,12 +1,43 @@
 function same(arr1, arr2) {
-  result = "";
   if (arr1.length !== arr2.length) return false;
+
+  sar1 = arr1.sort();
+  sar2 = arr2.sort();
+
   for (let i = 0; i < arr1.length; i++) {
-    arr1[i] !== Math.sqrt(arr2[i]) ? (result = false) : (result = true);
+    if (sar1[i] !== Math.sqrt(sarr2[i])) {
+      return false;
+    } else {
+      return true;
+    }
   }
-  return result;
 }
 
+
+function buildMap(arr) {
+  map = {};
+  for (item of arr) {
+    map[item] = map[item] + 1 || 1;
+  }
+  return map;
+}
+
+function same2(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  map1 = buildMap(arr1);
+  map2 = buildMap(arr2);
+  console.log(map1);
+  console.log(map2);
+
+  for (let number in map1) {
+    if (map2[number] !== Math.pow(number, 2)) {
+      console.log(map2[number], number);
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
 // arr1 = [1, 2, 3];
 // arr2 = [1, 4, 9];
 // arr3 = [1, 5, 6];
